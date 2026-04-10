d733a87089f46633987e7d46da7e10054145cdea
yavuzselim1231 <yavuzselim.eser@bahcesehir.edu.tr>
2026-04-10 18:05:48 +0300

    Update MainProgram.cpp

--- MainProgram.cpp
+++ MainProgram.cpp
@@ -84,7 +84,7 @@ public:
 
 MyString::MyString() {
     // TODO: Initialize with empty string
-    data="";
+    data=" ";
 }
 
 MyString::MyString(const string& str) {
@@ -115,8 +115,8 @@ int MyString::length() const {
 char MyString::charAt(int index) const {
     // TODO: Return character at given index
     // Throw std::out_of_range if index is invalid (negative or >= length)
-    if ( index< 0 || index > static_cast<int>(data.length())){
-        throw out_of_range ("İndex out of range in charAt");
+    if ( index< 0 || index >= static_cast<int>(data.length())){
+        throw out_of_range("İndex out of range in charAt");
     }
     return data[index];
 }
